82d528389d45076b03af4adf979e2cdd86ac6738
tayyab-004 <tayyabgee333@gmail.com>
2025-01-31 12:16:24 +0500

    Fix path name for configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
