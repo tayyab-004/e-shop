3a108d0265efd8bec9b9c2409264d73c124d0ab7
tayyab-004 <tayyabgee333@gmail.com>
2025-10-10 18:55:57 +0500

    Bug fixes

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    origin: [process.env.CLIENT_ORIGIN, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
